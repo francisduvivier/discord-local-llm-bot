39f7ba737eaa7d9ddb69f237412aa9e03daaa99a
Francis Duvivier <francisduvivier@gmail.com>
2023-10-23 21:50:54 +0200

    Login working

--- index.ts
+++ index.ts
@@ -1,1 +1,22 @@
-console.log("Hello via Bun!");
+import {Client} from "discord.js";
+
+const client = new Client({
+    intents: ["Guilds", "DirectMessages", "GuildMessages"], // Set the intents for your bot
+});
+
+
+async function startBot() {
+    client.on("ready", () => {
+        console.log(`Logged in as ${client.user?.tag}!`);
+    });
+
+    client.on("message", (message) => {
+        if (message.author === client.user) return; // Don't respond to messages sent by the bot itself
+
+        // Write your message here
+        console.log(`Received message: ${message.content}`);
+    });
+    await client.login("MTE2Mzk1MTkwMDg0NTE2MjU4Ng.GHlJ0g.ffW2c6LAQYZzxBqaAb2cpBIu7FsNMrEe0-Ptfs"); // Replace with your bot token
+}
+
+startBot();
