417deaf0f07d201b507b9fbb748217e5a124627e
Francis Duvivier <francisduvivier@gmail.com>
2023-10-23 21:50:55 +0200

    add streaming logs

--- src/langchain-ollama.ts
+++ src/langchain-ollama.ts
@@ -1,10 +1,19 @@
 import {Ollama} from "langchain/llms/ollama";
 import {ChatOllama} from "langchain/chat_models/ollama";
+import { CallbackManager } from "langchain/callbacks";
 
 // Llama 2 7b wrapped by Ollama
 const model = new Ollama({
     baseUrl: "http://localhost:11434",
     model: "mistral",
+    callbackManager: CallbackManager.fromHandlers({
+        async handleLLMNewToken(token: string) {
+          console.log({ token });
+        },
+        async handleLLMEnd(output) {
+          console.log("End of stream.", output);
+        }
+    })
 });
 
 const chatModel = new ChatOllama(model);
