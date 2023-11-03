1fe8084154148be11e21e85566e4aad3716e49d5
Francis Duvivier <francisduvivier@gmail.com>
2023-11-03 23:22:12 +0100

    Implement ollama.py without streaming

--- discordbot/bot.py
+++ discordbot/bot.py
@@ -7,9 +7,11 @@ import re
 
 load_dotenv()
 DISCORD_BOT_TOKEN = os.getenv('DISCORD_BOT_TOKEN')
-DISCORD_ANNOUNCEMENT_CHANNELS = os.getenv('DISCORD_ANNOUNCEMENT_CHANNELS').split(',') if os.getenv('DISCORD_ANNOUNCEMENT_CHANNELS') else []
+DISCORD_ANNOUNCEMENT_CHANNELS = os.getenv('DISCORD_ANNOUNCEMENT_CHANNELS').split(',') if os.getenv(
+    'DISCORD_ANNOUNCEMENT_CHANNELS') else []
 DISCORD_ANNOUNCEMENT_CHANNELS = list(map(int, DISCORD_ANNOUNCEMENT_CHANNELS))
-DISCORD_ANSWER_CHANNELS = os.getenv('DISCORD_ANSWER_CHANNELS').split(',') if os.getenv('DISCORD_ANSWER_CHANNELS') else []
+DISCORD_ANSWER_CHANNELS = os.getenv('DISCORD_ANSWER_CHANNELS').split(',') if os.getenv(
+    'DISCORD_ANSWER_CHANNELS') else []
 DISCORD_ANSWER_CHANNELS = list(map(int, DISCORD_ANSWER_CHANNELS))
 
 intents = discord.Intents.default()
@@ -18,11 +20,13 @@ intents.messages = True
 
 bot = commands.Bot(command_prefix='!', intents=intents)
 
+
 def is_supported_channel(channel_id):
     if len(DISCORD_ANSWER_CHANNELS) == 0:
         # Allow all channels if the channel list is empty
         return True
     return channel_id in DISCORD_ANSWER_CHANNELS
+
 
 @bot.event
 async def on_ready():
@@ -30,8 +34,11 @@ async def on_ready():
     for guild in bot.guilds:
         for channel in guild.channels:
             if channel.id in DISCORD_ANNOUNCEMENT_CHANNELS:
-                message = await channel.send("Hello, LLM_PLAYGROUND_CHANNEL! I just started up.")
-                await message.edit(content='Hello, LLM_PLAYGROUND_CHANNEL! I just started up. I can edit messages.')
+                startupAnnouncement = "Local LLM Discord Bot starting up ..."
+                message = await channel.send(startupAnnouncement)
+                await message.edit(content=startupAnnouncement + '\nEdit: Model says: ' + get_answer(
+                    'You are a discord bot on an awesome Maker Space discord guild, write a startup message to announce your presence in the channel.'))
+
 
 @bot.event
 async def on_message(message):
@@ -45,12 +52,13 @@ async def on_message(message):
 
     if bot.user.mentioned_in(message):
         pattern = '(?P<salute>[^@]*)<@.*>[^a-zA-Z]*(?P<question>.*)'
-        (salute, question)  = re.search(pattern, message.content).groups()
+        (salute, question) = re.search(pattern, message.content).groups()
 
         print(f'extracted question: {question}')
         start_reply = f'{salute}<@{message.author.id}>, '
         answer_message = await message.reply(start_reply)
         llm_answer = get_answer(question)
-        await answer_message.edit(content=start_reply+llm_answer)
+        await answer_message.edit(content=start_reply + llm_answer)
+
 
 bot.run(DISCORD_BOT_TOKEN)
--- discordbot/ollama.py
+++ discordbot/ollama.py
@@ -1,2 +1,15 @@
-def get_answer(input):
-    return input
+from langchain.chat_models import ChatOllama
+from langchain.callbacks.manager import CallbackManager
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+
+chat_model = ChatOllama(
+    model="mistral",
+    callback_manager=CallbackManager([StreamingStdOutCallbackHandler()]),
+)
+
+def get_answer(question):
+    try:
+       result = chat_model.predict(question)
+       return result
+    finally:
+       pass
