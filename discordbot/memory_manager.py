37bf337e2d59471407aab6eccc80b9f80c14d5ff
Francis Duvivier <francisduvivier@gmail.com>
2023-11-07 20:06:41 +0100

    Fix reading embedded text of bot

--- discordbot/memory_manager.py
+++ discordbot/memory_manager.py
@@ -12,10 +12,13 @@ async def get_message_history(message: discord.Message) -> List[BaseMessage]:
         referenced_message_id = current_message.reference.message_id
         channel = current_message.channel
         referenced_message = await channel.fetch_message(referenced_message_id)
+        message_content = referenced_message.clean_content
+        for embed in referenced_message.embeds:
+            message_content += embed.description
         if referenced_message.author.bot:
-            messages.append(AIMessage(content=referenced_message.content))
+            messages.append(AIMessage(content=message_content))
         else:
-            messages.append(HumanMessage(content=referenced_message.content))
+            messages.append(HumanMessage(content=message_content))
         current_message = referenced_message
 
     # Last appended messages should be first in the list since they are the oldest
