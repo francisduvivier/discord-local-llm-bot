7874444b2650b6f4799f23c183e35f18e5585f31
Francis Duvivier <francisduvivier@gmail.com>
2023-11-07 20:02:23 +0100

    Fixup role in message history

--- discordbot/memory_manager.py
+++ discordbot/memory_manager.py
@@ -12,7 +12,7 @@ async def get_message_history(message: discord.Message) -> List[BaseMessage]:
         referenced_message_id = current_message.reference.message_id
         channel = current_message.channel
         referenced_message = await channel.fetch_message(referenced_message_id)
-        if current_message.author.bot:
+        if referenced_message.author.bot:
             messages.append(AIMessage(content=referenced_message.content))
         else:
             messages.append(HumanMessage(content=referenced_message.content))
