c665ce8ed2e4d042e5c153743d9b1fc069603a22
Francis Duvivier <francisduvivier@gmail.com>
2023-11-18 10:19:25 +0100

    Add script that can be run from cronjob to updat the bot

---
+++ update-on-git-change.sh
@@ -0,0 +1,12 @@
+SCRIPT_DIR=$(dirname $0)
+cd $SCRIPT_DIR
+git stash -m "local stuff"
+
+if git pull | grep "Already up to date"
+then
+    echo "Up to date, not doing anything!"
+else
+    killall python;
+    poetry run python discordbot 2> log.error.ignorethis.txt 1> log.ignorethis.txt &
+fi
+
