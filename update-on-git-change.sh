0623818245c89eb08f336b4a68e96e265c2cdf1e
Francis Duvivier <francisduvivier@gmail.com>
2023-11-18 10:39:26 +0100

    Fix killing previous python process

--- update-on-git-change.sh
+++ update-on-git-change.sh
@@ -6,8 +6,10 @@ if git pull | grep "Already up to date"
 then
     echo "Up to date, not doing anything!"
 else
-    killall python || echo 'no prob';
-    poetry run python discordbot 2> log.error.ignorethis.txt 1> log.ignorethis.txt &
+    killall python || echo 'python not running yet';
+    killall Python || echo 'Python not running yet';
+    poetry install;
+    poetry run python discordbot 2>log.error.ignorethis.txt 1>log.ignorethis.txt &
     echo "restarted discordbot"
 fi
 
