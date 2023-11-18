6db8aeacb56a61c3d0223eb7712aff3b99d96785
Francis Duvivier <francisduvivier@users.noreply.github.com>
2023-11-18 16:03:02 +0100

    Update update-on-git-change.sh to use screen

--- update-on-git-change.sh
+++ update-on-git-change.sh
@@ -1,7 +1,7 @@
 source ~/.bash_profile
 killall caffeinate || echo 'ok';
 caffeinate &;
-ssh op5b.local '/home/fduvivier/planmail/send.sh botok' || echo 'ok';
+
 SCRIPT_DIR=$(dirname $0)
 cd $SCRIPT_DIR
 git stash -m "local stuff"
@@ -13,7 +13,8 @@ else
     killall python || echo 'python not running yet';
     killall Python || echo 'Python not running yet';
     poetry install;
-    poetry run python discordbot 2>log.error.ignorethis.txt 1>log.ignorethis.txt &
+    screen -S bot -X quit || echo 'no prob';
+    screen -dmS bot sh -c 'poetry run python discordbot 2>log.error.ignorethis.txt 1>log.ignorethis.txt';
     echo "restarted discordbot"
 fi
 
