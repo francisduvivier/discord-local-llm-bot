6aab0d431690173664e19b559cd256024aab9eac
Francis Duvivier <francisduvivier@users.noreply.github.com>
2023-11-18 16:13:42 +0100

    Update update-on-git-change.sh

--- update-on-git-change.sh
+++ update-on-git-change.sh
@@ -1,6 +1,6 @@
 source ~/.bash_profile
-killall caffeinate || echo 'ok';
-caffeinate &;
+screen -S caffeinate -X quit || echo 'no prob';
+screen -dmS caffeinate -c 'caffeinate';
 
 SCRIPT_DIR=$(dirname $0)
 cd $SCRIPT_DIR
@@ -16,5 +16,6 @@ else
     screen -S bot -X quit || echo 'no prob';
     screen -dmS bot bash -c 'source ~/.bash_profile && poetry run python discordbot 2>log.error.ignorethis.txt 1>log.ignorethis.txt';
     echo "restarted discordbot"
+    ssh 192.168.0.55 'echo restarted discordbot' || echo 'no prob';
 fi
 
